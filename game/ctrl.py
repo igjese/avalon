ba5a094eb31172c43e3908c115b46776b121253a
Igor Jese <igor@mockupscreens.com>
2023-10-03 10:03:08 +0200

    print component in/out to console

--- game/ctrl.py
+++ game/ctrl.py
@@ -23,20 +23,32 @@ def process_subsystem(sub_system):
 
 def process_installed_component(installed_component):
     component = installed_component.component
+    sub_system = installed_component.parent_subsystem
+    ship_system = sub_system.parent_system
     consumes = component.consumes
     produces = component.produces
+
+    consume_text = ""
+    produce_text = ""
 
     # Handle consumption
     for resource_name, amount in consumes.items():
         resource = Resource.objects.get(name=resource_name)
         resource.quantity -= amount  # Now consumes for just one instance
         resource.save()
+        consume_text += f"{amount} {resource_name}, "
 
     # Handle production
     for resource_name, amount in produces.items():
         resource = Resource.objects.get(name=resource_name)
         resource.quantity += amount  # Now produces for just one instance
         resource.save()
+        produce_text += f"{amount} {resource_name}, "
+
+    consume_text = consume_text[:-2] if consume_text else "-"
+    produce_text = produce_text[:-2] if produce_text else "-"
+
+    print(f"{component.name} in {installed_component.parent_subsystem}/{installed_component.parent_subsystem.parent_system} | In: {consume_text} | Out: {produce_text}")
 
 def get_game_state():
     # Your logic to collect and return the current state of the game

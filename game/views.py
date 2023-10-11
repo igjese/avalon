578362aade7021fe0e0c2e9c4249d459e97469ff
Igor Jese <igor@mockupscreens.com>
2023-10-11 15:25:45 +0200

    refactor: preparing presentation data in single place

--- game/views.py
+++ game/views.py
@@ -13,61 +13,54 @@ WATER_THRESHOLDS = {'Green': 81, 'Yellow': 51, 'Red': 0}
 FOOD_THRESHOLDS = {'Green': 81, 'Yellow': 51, 'Red': 0}
 ENERGY_THRESHOLDS = {'Green': 81, 'Yellow': 51, 'Red': 0}
 
-
 # Create your views here.
 def index(request):
-    resources = Resource.objects.all()
-    systems = ShipSystem.objects.all()
-    components = Component.objects.all()
-    storage_types = StorageType.objects.all()
-    storage_units = StorageUnit.objects.all()
-    installed_storage_units = InstalledStorageUnit.objects.all()
-    stored_resources = StoredResource.objects.all()
-    game_state = ctrl_get_game_state()
-    current_tick = World.objects.get(pk=1).current_tick
-
-    return render(request, 'game/index.html', {
-        'resources': resources,
-        'systems': systems,
-        'components': components,
-        'storage_types': storage_types,
-        'storage_units': storage_units,
-        'installed_storage_units': installed_storage_units,
-        'stored_resources': stored_resources,
-        'ship_resources': game_state['resources'],
-        'history': json.dumps(game_state['history']),
-        'alerts': get_alerts(game_state['resources']),
-        'game_time': calculate_gametime(current_tick),
-    })
-
+    context = prepare_context()
+    return render(request, 'index.html', context)
 
 def advance_game_tick_and_get_game_state(request):
-    ctrl.advance_game_tick()  # Advance the game by one tick
-    game_state = ctrl_get_game_state()  # Get the current game state
-
-    # Prepare the alerts data
-    game_state['alerts'] = get_alerts(game_state['resources'])
-
-    # Calculate game time
-    game_state['game_time'] = calculate_gametime(game_state['current_tick'])
-
-    # Transform history data to have game time instead of ticks
-    transformed_history = []
-    for entry in game_state['history']:
-        transformed_entry = entry.copy()
-        transformed_entry['game_time'] = calculate_short_gametime(entry['tick'])
-        del transformed_entry['tick']
-        transformed_history.append(transformed_entry)
-
-    game_state['history'] = transformed_history
-
-    return JsonResponse(game_state)
+    ctrl.advance_game_tick()
+    context = prepare_context()
+    return JsonResponse(context)
 
 def restart_game(request):
     ctrl.restart_game()
     return HttpResponseRedirect(reverse('main'))
 
-def get_alerts(ship_resources):
+def prepare_context():
+    # Prepare context
+    context = {
+        'resources': Resource.objects.all(),
+        'systems': ShipSystem.objects.all(),
+        'subsystems': SubSystem.objects.all(),
+        'components': Component.objects.all(),
+        'storage_types': StorageType.objects.all(),
+        'storage_units': StorageUnit.objects.all(),
+        'installed_storage_units': InstalledStorageUnit.objects.all(),
+        'installed_components': InstalledComponent.objects.all(),
+        'stored_resources': StoredResource.objects.all(),
+        'alerts': get_alerts(),
+        'game_time': calculate_gametime(World.objects.get(pk=1).current_tick),
+        'resource_data': get_resource_data(),
+        'history_data': list(ResourceHistory.objects.values('tick', 'quantity_data', 'production_data', 'consumption_data')),
+    }
+
+    return context
+
+def get_resource_data():
+    from .ctrl import aggregated
+    resource_data = {}
+
+    for resource in Resource.objects.all():
+        if resource.name not in resource_data:
+            resource_data[resource.name] = {}
+
+        resource_data[resource.name]['available'] = aggregated.available_amount[resource.name]
+        resource_data[resource.name]['capacity'] = aggregated.total_capacity[resource.name]
+
+    return resource_data
+
+def get_alerts():
     alerts = {}
     thresholds = {
         'Air': AIR_THRESHOLDS,
@@ -75,6 +68,7 @@ def get_alerts(ship_resources):
         'Food': FOOD_THRESHOLDS,
         'Energy': ENERGY_THRESHOLDS,
     }
+    ship_resources = get_resource_data()
 
     for resource_name, threshold in thresholds.items():
         available = ship_resources[resource_name]['available']
@@ -114,25 +108,3 @@ def calculate_short_gametime(tick_count):
     short_time += f"{hours}:{str(minutes).zfill(2)}"
 
     return short_time
-
-def ctrl_get_game_state():
-    from .ctrl import aggregated
-    # Collect current resources data
-    resources_data = {}
-    for resource in Resource.objects.all():
-        if resource.name not in resources_data:
-            resources_data[resource.name] = {}
-        resources_data[resource.name]['available'] = aggregated.available_amount[resource.name]
-        resources_data[resource.name]['capacity'] = aggregated.total_capacity[resource.name]
-
-    # Fetch the resource history data
-    history_data = list(ResourceHistory.objects.values('tick', 'quantity_data', 'production_data', 'consumption_data'))
-
-    # Add any additional game state information you may have
-    game_state = {
-        'resources': resources_data,
-        'history': history_data,
-        'current_tick': World.objects.get(pk=1).current_tick
-    }
-
-    return game_state
